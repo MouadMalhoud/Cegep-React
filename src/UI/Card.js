58b28d7a54fe3b2979e8e0a0b4d371e5a3530658
Mouad Malhoud <mouad-professionel@hotmail.com>
2023-03-16 12:36:02 -0400

    Avancement card.js et css

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import { useState } from 'react';
 import './Card.css';
 
 const Card = (props) => {
-  
-  console.log(props.date)
+  const localprofs = localStorage.getItem(props.key);
+  const [profs, setProfs] = useState(JSON.parse(localprofs));
+
+  console.log(profs)
+
   return (
     <div className='card'>
-        
+         
       <img width={70} src={props.photo} alt='Erreur de chargement'/>
       <div className='card-info'>
         <h3>nom : {props.nom}</h3> 
